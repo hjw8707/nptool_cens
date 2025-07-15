7752deb76a2897f3baeb98fa6a129da66553a35e
Jongwon Hwang <hjw8707@gmail.com>
2025-07-15 16:56:45 +0900

    STARK 물리 클래스 및 헤더 파일 수정: TSTARKPhysics 클래스의 생성자 및 메서드 정리, 불필요한 코드 제거 및 주석 추가. STARK 시뮬레이션 관련 파일에서 그룹 파라미터 추가 및 배치 매크로에서 시뮬레이션 이벤트 수 조정. 또한, 여러 파일에서 코드 포맷팅 개선. dE-E 분석 관련 코드 추가.

--- Projects/STARK/script_alpha.sh
+++ Projects/STARK/script_alpha.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-
+#!/bin/zsh
 ##### for simplicity, only 1000 events ####
-tee > batch.mac <<EOF
-/run/beamOn 1000
-EOF
+#tee >batch.mac <<EOF
+#/run/beamOn 10
+#EOF
 ###########################################
 
-for x in proton deuton triton alpha
-do
-    echo "simulation for $x"
-    npsimulation -D detectors/stark_full.det -E sources/$x.source -O stark_$x.root -B batch.mac
-    npanalysis -T ../../Outputs/Simulation/stark_$x.root SimulatedTree -O stark_$x.root
-done
+#for x in alpha; do #proton deuton triton alpha
+#    echo "simulation for $x"
+export >export.txt
+x=alpha
+npsimulation -D detectors/stark_full.det -E sources/$x.source -O stark_$x.root -B batch.mac
+npanalysis --last-sim -O stark_$x.root
+#done
 
-cd ../../Outputs/Analysis
-hadd -f stark_pdta.root stark_proton.root stark_deuton.root stark_triton.root stark_alpha.root
-cd ../../Projects/STARK
+#cd ../../Outputs/Analysis
+#hadd -f stark_pdta.root stark_proton.root stark_deuton.root stark_triton.root #stark_alpha.root
+#cd ../../Projects/STARK
 
-root macro/stark_pdta.C 
+#root macro/stark_pdta.C
